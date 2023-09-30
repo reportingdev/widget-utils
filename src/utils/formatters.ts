64b6a31cd21eb4c7975eef7f7d1f5ac8f4ab8298
Braden Ericson <bradenericson@users.noreply.github.com>
2023-09-29 23:54:14 -0500

    added date and currency formatters

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,4 +1,6 @@
 import { isFinite } from "lodash";
+import { enUS, enGB,es,fr,de,it,nl,ru,zhCN,ja,ko,ar,hi,pt,sv } from 'date-fns/locale';
+import {format} from 'date-fns'
 
 const percentageFormatter = (value:number) => {
   
@@ -53,13 +55,96 @@ const shorthandDates = (dateString:string) => {
     return shortDate;
 };
 
-function formatCurrency(amount: number, currency:string="USD"): string {
-  const formatter = new Intl.NumberFormat('en-US', {
+type SupportedLocales = 'en-US' | 'en-GB' | 'es' | 'fr' | 'de' | 'it' | 'nl' | 'ru' | 'zh-CN' | 'ja' | 'ko' | 'ar' | 'hi' | 'pt' | 'sv';
+type SupportedCurrencies = 'USD' | 'GPB' | 'EUR' | 'CNY' | 'JPY' | 'KRW' | 'INR' | 'SEK';
+
+function formatCurrency(amount: number, currency:SupportedCurrencies="USD", locale:SupportedLocales='en-US'): string {
+  const formatter = new Intl.NumberFormat(locale, {
     style: 'currency',
     currency: currency,
   });
 
   return formatter.format(amount);
+}
+
+type ParsedCurrency = {
+  amount: number;
+  currencyCode: SupportedCurrencies;
+};
+
+function parseCurrencyString(currencyString: string):ParsedCurrency | null {
+  // Using regex to capture amount and currency symbol
+  const regex = /([^\d]*)([\d.,]+)/;
+  const matches = currencyString.match(regex);
+  
+  if (!matches) return null;
+
+  const [, currencySymbol, amountString] = matches;
+
+  // Remove commas and convert string to number
+  const amount = parseFloat(amountString.replace(/,/g, ''));
+
+  // Mapping currency symbols to currency codes
+  const symbolToCode: { [key: string]: string } = {
+    '$': 'USD',
+    '£': 'GBP',
+    '€': 'EUR',
+    '¥': 'JPY',
+    'CY¥': 'JPY',
+    '₩': 'KRW',
+    '₽': 'RUB',
+    '₹': 'INR',
+    'R$': 'BRL',
+    'SEK': 'SEK',
+    // Add more mappings as needed
+  };
+
+  const currencyCode = symbolToCode[currencySymbol.trim()] || 'Unknown';
+
+  return {
+    amount,
+    currencyCode,
+  };
+};
+
+type DateFormat = 'localized' | 'us' | 'us-short' | 'european' | 'european-short' | 'asian';
+
+function formatDate(date:Date | string, locale:SupportedLocales='en-US', dateFormat:DateFormat='localized'):string {
+  const dateObj = typeof date === 'string' ? new Date(date) : date;
+  const localeMap = {
+    'en-US': enUS,
+    'en-GB': enGB,
+    'es': es,
+    'fr': fr,
+    'de': de,
+    'it': it,
+    'nl': nl,
+    'ru': ru,
+    'zh-CN': zhCN,
+    'ja': ja,
+    'ko': ko,
+    'ar': ar,
+    'hi': hi,
+    'pt': pt,
+    'sv': sv,
+  };
+
+  switch (dateFormat) {
+    case 'localized':
+      return format(dateObj, 'PP', { locale: localeMap[locale] ?? enUS });
+    case 'us':
+      return format(dateObj, 'MM/dd/yyyy');
+    case 'european':
+      return format(dateObj, 'dd/MM/yyyy');
+    case 'us-short':
+      return format(dateObj, 'MM/dd');
+    case 'european-short':
+      return format(dateObj, 'dd/MM');
+    case 'asian':
+      return format(dateObj, 'yyyy/MM/dd');
+    default:
+      return format(dateObj, 'PP', { locale: localeMap[locale] ?? enUS });
+  }
 }
 
 
@@ -69,5 +154,7 @@ export {
   abbreviateNumber,
   roundToTwo,
   shorthandDates,
-  formatCurrency
+  formatCurrency,
+  formatDate,
+  parseCurrencyString
 };
