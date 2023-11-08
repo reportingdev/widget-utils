ef1412fc820754bddf16d3408720426d18eef786
Braden Ericson <bradenericson@users.noreply.github.com>
2023-11-08 14:12:54 -0600

    add serialized date method + tests

--- src/tests/localization.test.ts
+++ src/tests/localization.test.ts
@@ -1,93 +1,56 @@
 import * as sinon from 'sinon';
-import { formatCurrency, getToday } from '../utils/localization';
+import { formatCurrency, serializeDate } from '../utils/localization';
 
-describe('getToday', () => {
+
+describe('serializeDate', () => {
   let clock: sinon.SinonFakeTimers;
-  let OriginalDate: DateConstructor;
 
-  function setDateObject(){
-    global.Date = class extends OriginalDate {
-      constructor() {
-        super();
-        return new OriginalDate(clock.now);
-      }
-  
-      static now() {
-        return clock.now; // this will ensure Date.now() is using the mocked time
-      }
-  
-      // static UTC(year: number, month: number, date?: number, hours?: number, minutes?: number, seconds?: number, ms?: number) {
-      //   // Adjust the arguments to account for any defaults.
-      //   const args = [year, month, date ?? 1, hours ?? 0, minutes ?? 0, seconds ?? 0, ms ?? 0];
-      //   // Use the apply function to call the original Date.UTC with the adjusted arguments
-      //   return OriginalDate.UTC.apply(null, args);
-      // }
-    } as unknown as DateConstructor;
-  }
-
-  beforeEach(() => {
-    OriginalDate = global.Date;
-    //clock = sinon.useFakeTimers(new Date('2023-11-07T23:30:00-06:00').getTime());
+  beforeAll(() => {
+    const fixedDate = new Date('2023-11-07T12:00:00Z');
+    clock = sinon.useFakeTimers(fixedDate.getTime());
   });
-  
-  afterEach(() => {
+
+  afterAll(() => {
     clock.restore();
-    global.Date = OriginalDate;
   });
 
-  it('should return the UTC date', () => {
-    clock = sinon.useFakeTimers(new Date('2023-11-07T11:30:00Z').getTime());
-    setDateObject();
-    const todayUTC = getToday(false);
-    expect(todayUTC.getDate()).toBe(7)
+  test('should serialize a date to ISO 8601 with timezone format correctly', () => {
+    const date = new Date();
+    const serialized = serializeDate(date, 'iso8601');
+    expect(serialized).toBe(date.toISOString());
   });
 
-  it('should return local date', () => {
-    clock = sinon.useFakeTimers(new Date('2023-11-07T11:30:00Z').getTime());
-    setDateObject();
-    const todayLocal = getToday(true);
-    expect(todayLocal.getDate()).toBe(7);
+  test('should serialize a date to local ISO Date without time correctly', () => {
+    const date = new Date();
+    const serialized = serializeDate(date, 'isoDateLocal');
+    expect(serialized).toBe(date.toISOString().split('T')[0]);
   });
 
-  it('should handle Daylight Saving Time transition correctly', () => {
-    // Example for PST transition:
-    // Mock the current time to a specific point
-    const mockedCurrentTime = new OriginalDate('2023-03-12T10:00:00Z').getTime();
-    clock = sinon.useFakeTimers(mockedCurrentTime);
-    setDateObject();
-    const dstTransitionDay = getToday(true);
-    expect(dstTransitionDay.getDate()).toBe(12); // Assuming the transition is on 12th March
+  test('should serialize a date to UTC ISO Date without time correctly', () => {
+    const date = new Date();
+    const utcDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    const serialized = serializeDate(date, 'isoDateUTC');
+    expect(serialized).toBe(utcDate.toISOString().split('T')[0]);
   });
 
-  it('should handle the time around midnight correctly', () => {
-    // Set the fake time to 11:59 PM UTC
-    const mockedCurrentTime = new OriginalDate('2023-11-07T23:59:00Z').getTime()
-    clock = sinon.useFakeTimers(mockedCurrentTime);
-    setDateObject();
-
-    // Override the global Date object
-    const justBeforeMidnightUTC = getToday(false);
-    expect(justBeforeMidnightUTC.getDate()).toBe(new Date(clock.now).getUTCDate());
-    
-    // Move 2 seconds ahead to cross midnight
-    clock.tick(2000);
-    const justAfterMidnightUTC = getToday(false);
-    expect(justAfterMidnightUTC.getDate()).toBe(new Date(clock.now).getUTCDate());
+  test('should serialize a date to Unix timestamp string correctly', () => {
+    const date = new Date();
+    const serialized = serializeDate(date, 'unixTimestamp');
+    expect(serialized).toBe(date.getTime().toString());
   });
 
-  it('should handle when UTC time is tomorrow but local time is today', () => {
-    // Mock the current time to a specific point
-    const mockedCurrentTime = new OriginalDate('2023-11-07T23:30:00-06:00').getTime();
-    clock = sinon.useFakeTimers(mockedCurrentTime);
-    setDateObject();
-  
-    const todayUTC = getToday(false);
-    expect(todayUTC.getUTCDate()).toBe(8); // Expecting the date to be 8th UTC
-  
-    const todayLocal = getToday(true);
-    expect(todayLocal.getDate()).toBe(7); // Expecting the local date to be 7th
+  test('should serialize a date to RFC 2822 string correctly', () => {
+    const date = new Date();
+    const serialized = serializeDate(date, 'rfc2822');
+    expect(serialized).toBe(date.toUTCString());
+  });
+
+  test('should throw an error for unsupported formats', () => {
+    const date = new Date();
+    expect(() => serializeDate(date, 'unsupportedFormat' as any)).toThrow('Unsupported format specified');
   });
 });
+
 
 describe('formatCurrency', () => {
   test('formats without abbreviation', () => {
